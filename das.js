900bcb47f067cff6895d663e256babdd97e3dab2
Eryk Wiśniewski <118454187+ErykHonda@users.noreply.github.com>
2024-02-15 16:17:27 +0000

    Copilot first commit

---
+++ das.js
@@ -0,0 +1,5 @@
+print("wor ")
+
+//git add das.js
+//git commit -m "Copilot first commit"
+//$ git push
--- skills.js
+++ skills.js
@@ -1,4 +1,10 @@
 function calculateNumbers(var1, var2)
 {
     console.log(var1 + var2);
+    console.log(123)
+    console.log(123)
 }
+
+//git add skills.js
+//git commit -m "Copilot first commit"
+//$ git push
